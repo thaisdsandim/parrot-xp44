27be653edcb80aa244b59ebba551a4dcf503ab97
Beatriz Vargas <beatrizvargas938@gmail.com>
2022-10-25 16:46:31 -0300

    First commit

---
+++ src/components/Login.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import BackgroundImage from '../../assets/cover.png'
+import Logo from '../../assets/logo.png'
+import {useNavigate} from 'react-router-dom'
+
+interface LoginFormProps {
+    setSignIn: () => void;
+}

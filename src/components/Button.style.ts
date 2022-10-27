21747338e45ff3d5c9a2aecef32b2e379da83417
Beatriz Vargas <beatrizvargas938@gmail.com>
2022-10-27 19:49:01 -0300

    Add botão

---
+++ src/components/Button.style.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const ButtonForm = styled.button`
+    width: 80%;
+    height: 39px;
+    background: #76BB4C;
+    border: none;
+    border-radius: 10px;
+    font-weight: 400;
+    font-size: 24px;
+    line-height: 25px;
+    margin-bottom: 1.5rem;
+    
+    @media only screen and (max-width: 600px){
+        width: 5rem;
+        font-size: 18px;
+        line-height: 19px;
+    }
+`
---
+++ src/components/Button.tsx
@@ -0,0 +1,21 @@
+import {ButtonHTMLAttributes} from "react";
+import{ButtonForm} from "./Button.style"
+
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>{
+    type: "submit" | "reset" | "button" ;
+    value: string;
+    text: string;
+}
+
+
+function Button ({type="submit", value="submit", text}:ButtonProps){
+    return(
+        <ButtonForm type={type} value={value}>{text}</ButtonForm>
+    )
+    
+}
+
+
+export default Button;
+
